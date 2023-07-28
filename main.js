779c0d8ba6f4598f76bad6da5886c72926808675
Fire_Ball_MC <lorenz.schlenker@gmail.com>
2023-07-28 15:26:12 +0200

    added some stuff

--- main.js
+++ main.js
@@ -2,13 +2,22 @@ const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 const FPS = 60
-const TPS = 120
+const TPS = 10000
 
-var leftText = new TextBlock(10, 20, 20)
+var leftText = new TextBlock(10, 20, 20, "white")
 var rightText = new TextBlock(200, 20, 20, "white")
+var planets = [new Planet(new Vector(canvas.width/2, canvas.height/2), new Vector(0, 0), 50, new PhysicsMaterial(1000), "yellow")]
 
 var fps = 0
 var tps = 0
+var mouseDown = false
+var mouseX = 0
+var mouseY = 0
+var bSize = 10
+var isPaused = false
+var displayVectors = false
+
+var G = 0.00001
 
 setInterval(tick, 1000 / TPS)
 setInterval(render, 1000 / FPS)
@@ -18,7 +27,12 @@ function tick(){
     deltaTime = performance.now() - tTime
     tTime = performance.now()
     tps = (tps * 9 + 1000 / deltaTime) / 10
-
+    if(isPaused){
+        return
+    }
+    for(let i = 0; i < planets.length; i++){
+        planets[i].step(planets)
+    }
 }
 rTime = 0
 function render(){
@@ -27,12 +41,100 @@ function render(){
     fps = (fps * 9 + 1000 / deltaTime) / 10
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.fillStyle = "black"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
     
 
     rightText.x = canvas.width - 100
     rightText.setText(0, "FPS: " + Math.round(fps.toFixed(2)))
     rightText.setText(1, "TPS: " + Math.round(tps.toFixed(2)))
 
+    leftText.setText(0, "Planets: " + planets.length)
+    leftText.setText(1, "G: " + G)
+    leftText.setText(2, "paused: " + isPaused)
+    leftText.setText(3, "show vectors: " + displayVectors)
+    leftText.setText(4, "bSize: " + bSize)
+
     rightText.draw(ctx)
     leftText.draw(ctx)
+
+    for(let i = 0; i < planets.length; i++){
+        // console.log(planets[i])
+        planets[i].draw(ctx)
+        if(displayVectors){
+            planets[i].drawVel(ctx)
+        }
+    }
+}
+
+document.addEventListener("mousedown", onMouseDown)
+document.addEventListener("mouseup", onMouseUp)
+document.addEventListener("mousemove", onMove)
+document.addEventListener("mousewheel", onMouseWheel)
+document.addEventListener("keydown", onKeyDown)
+document.addEventListener("contextmenu", function(e){e.preventDefault()})
+
+function onMouseDown(e){
+    // console.log(e)
+    if(e.button == 0){
+        mouseDown = true
+    }
+}
+
+function onMouseUp(e){
+    // console.log(e)
+    if(e.button == 0 && mouseDown){
+        onclick(e)
+    }
+    if(e.button == 0){
+        mouseDown = false
+    }
+}
+
+function onclick(e){
+    // console.log("click", e.clientX, e.clientY)
+    // console.log(planets)
+    planets.push(new Planet(new Vector(e.clientX, e.clientY), new Vector(5, 0), bSize, new PhysicsMaterial(1)))
+}
+
+function onMove(e){
+    mouseX = e.clientX
+    mouseY = e.clientY
+}
+
+function onMouseWheel(e){
+    // console.log("wheel", e.deltaY)
+    istrue = false
+    istrue = leftText.mouseAction(1, textBlockMWheel, e, true)
+    if(!istrue){
+        bSize -= e.deltaY / 10
+    }
+}
+
+function onKeyDown(e){
+    console.log(e)
+    switch(e.key){
+        case " ":
+            console.log("space")
+            isPaused = !isPaused
+            break
+        case "h":
+            displayVectors = !displayVectors
+            break
+    }
+}
+
+function textBlockMWheel(e, index){
+    var modifier = 1;
+    if (e.shiftKey) {
+        modifier = 0.1;
+    }
+    if (e.altKey) {
+        modifier = 0.01;
+    }
+    switch(index){
+        case 1:
+            // console.log("G", e.deltaY)
+            G -= e.deltaY * modifier / 100
+            break
+    }
 }
--- objects.js
+++ objects.js
@@ -1,18 +1,36 @@
 class Planet{
-    constructor(x, y, radius, physMat, color = "blue", gravity = 100){
-        this.x = x
-        this.y = y
+    constructor(pos, vel, radius, physMat, color = "blue"){
+        this.pos = pos
+        this.vel = vel
         this.radius = radius
         this.physMat = physMat
         this.color = color
-        this.gravity = gravity
+        this.V = 4/3 * Math.PI * Math.pow(this.radius, 3)
+        this.M = this.V * this.physMat.density
     }
-    draw(){
-        drawCircle(this.x, this.y, this.radius, this.color)
+    draw(ctx){
+        // console.log("draw")
+        // console.log(this)
+        drawCircle(ctx, this.pos.x, this.pos.y, this.radius, this.color)
     }
     step(planets = null){
         if (planets == null)
             return
+        for (let i = 0; i < planets.length; i++){
+            if(planets[i] != this){
+                    // console.log(this)
+                    this.r = this.pos.distance(planets[i].pos)
+                    this.F = G *(this.M * planets[i].M) / Math.pow(this.r, 2)
+                    this.dir = this.pos.sub(planets[i].pos).normalize()
+                    this.vel = this.vel.add(this.dir.mult(this.F / this.M))
+                    // console.log(this.F)
+            }
+        }
+        this.pos = this.pos.sub(this.vel)
+    }
+    drawVel(ctx){
+        this.display = new VectorDisplay(this.pos.x, this.pos.y, this.vel.x * -10, this.vel.y * -10)
+        this.display.draw(ctx)
     }
 }
 
@@ -55,6 +73,88 @@ class Text {
     }
 }
 
+class Vector{
+    constructor(x, y){
+        this.x = x
+        this.y = y
+    }
+    add(v){
+        return new Vector(this.x + v.x, this.y + v.y)
+    }
+    sub(v){
+        return new Vector(this.x - v.x, this.y - v.y)
+    }
+    mult(s){
+        return new Vector(this.x * s, this.y * s)
+    }
+    div(s){
+        return new Vector(this.x / s, this.y / s)
+    }
+    mag(){
+        return Math.sqrt(this.x * this.x + this.y * this.y)
+    }
+    distance(v){
+        return this.sub(v).mag()
+    }
+    normalize(){
+        return this.div(this.mag())
+    }
+    /**
+     * @returns (0, 0)
+     */
+    static zero(){
+        return new Vector(0, 0)
+    }
+    /**
+     * @returns (0, -1)
+     */
+    static up(){
+        return new Vector(0, -1)
+    }
+    /**
+     * @returns (0, 1)
+     */
+    static down(){
+        return new Vector(0, 1)
+    }
+    /**
+     * @returns (-1, 0)
+     */
+    static left(){
+        return new Vector(-1, 0)
+    }
+    /**
+     * @returns (1, 0)
+     */
+    static right(){
+        return new Vector(1, 0)
+    }
+    /**
+     * @returns (-1, -1)
+     */
+    static upLeft(){
+        return new Vector(-1, -1)
+    }
+    /**
+     * @returns (1, -1)
+     */
+    static upRight(){
+        return new Vector(1, -1)
+    }
+    /**
+     * @returns (-1, 1)
+     */
+    static downLeft(){
+        return new Vector(-1, 1)
+    }
+    /**
+     * @returns (1, 1)
+     */
+    static downRight(){
+        return new Vector(1, 1)
+    }
+}
+
 class TextBlock {
     text = []
     constructor(x, y, gap, color = "black") {
@@ -76,14 +176,18 @@ class TextBlock {
         this.text[index].text = str
     }
     mouseAction(index, func, event, passIndex = false){
+        // console.log("mouseAction", index, func, event, passIndex)
         if(mouseY > this.y + this.gap * (index-1) && mouseY < this.y + this.gap * index){
             if (passIndex){
+                // console.log("passing index")
                 func(event, index)
             }
             else{
                 func(event)
             }
+            return true
         }
+        return false
     }
 }
 
@@ -99,7 +203,8 @@ function canvas_arrow(context, fromx, fromy, tox, toy) {
     context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6))
   }
 
-function drawCircle(x, y, radius, color = "red"){
+function drawCircle(ctx, x, y, radius, color = "red"){
+    // console.log(x, y, radius, color)
     ctx.beginPath()
     ctx.arc(x, y, radius, 0, 2*Math.PI)
     ctx.fillStyle = color
